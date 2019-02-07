dc5feac976d4f67156a5597786591abb12b7c373
Shihab Uddin <shihab.apps.developer@gmail.com>
2019-02-07 21:28:37 +0600

    added marker

--- GoogleMapDemo/ViewController.swift
+++ GoogleMapDemo/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
-
+import GoogleMaps
 class ViewController: UIViewController {
 
+//    AIzaSyCXgEDgiNxGqEumJsZD1HdFpmLsMsP90hs
+    
+    let latlong = 23.8103
+    let longitude = 90.4125
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera =  GMSCameraPosition.camera(withLatitude: latlong, longitude: longitude, zoom: 15)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera:  camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: latlong, longitude: longitude)
+        marker.title = "Dhaka"
+        marker.snippet = "Bangladesh"
+        marker.map = mapView
+
     }
 
     override func didReceiveMemoryWarning() {
